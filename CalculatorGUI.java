f45b79555c35feb420c50442eb55d90dad937ea8
Joe Oakes <joe.oakes@gmail.com>
2023-10-03 20:06:19 -0400

    Update CalculatorGUI.java

--- CalculatorGUI.java
+++ CalculatorGUI.java
@@ -1,27 +1,33 @@
+//import the following packages and classes
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+//Inheritance JFrame is the Parent class
+//CalculatorGUI is the child
 public class CalculatorGUI extends JFrame {
+    //Class level variables
+    //Accessible to only methods in this class
     private JTextField display;
     private double num1 = 0;
     private String operator = "";
     private boolean startNewInput = true;
 
+    //Constructor method for this class
     public CalculatorGUI() {
-        setTitle("Calculator");
-        setSize(300, 400);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+        setTitle("Calculator");  //Set the window frame text title bar
+        setSize(300, 400);       //Set the window frame dimensions 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //Set the window frame for what happens when closed
+        setLocationRelativeTo(null); //Set the window frame location
 
-        display = new JTextField(20);
-        display.setEditable(false);
-        display.setHorizontalAlignment(JTextField.RIGHT);
-        add(display, BorderLayout.NORTH);
+        display = new JTextField(20);  //Display (object) which is field for numbers selected
+        display.setEditable(false);    //set the display field for display only
+        display.setHorizontalAlignment(JTextField.RIGHT); //set the horiz align
+        add(display, BorderLayout.NORTH); //Add display(object) to the window frame
 
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(4, 4));
+        JPanel buttonPanel = new JPanel(); //New button Panel Object
+        buttonPanel.setLayout(new GridLayout(4, 4)); //Set the button Panel dimensions
 
         String[] buttonLabels = {
                 "7", "8", "9", "/",
